5311702fbadcb597e7c7a5d8a48a314bce0df5a8
Dhawal Parkar <dhawalparkar@gmail.com>
2018-02-19 21:15:49 -0800

    adding listing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from "react";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
+import '../styles/log-listing.css';
 
-const IndexPage = () => (
-  <div>
+export default function Index({ data }) {
+  const { edges: posts } = data.allMarkdownRemark;
+  return (
+    <div className="blog-posts">
     <Helmet
       title="Gatsby Default Starter"
       meta={[
@@ -22,11 +25,36 @@ const IndexPage = () => (
         { name: 'twitter:creator', content: '@dparkar'}
       ]}
     />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
-
-export default IndexPage
+      {posts
+        .filter(post => post.node.frontmatter.title.length > 0)
+        .map(({ node: post }) => {
+          return (
+            <div className="blog-post-preview" key={post.id}>
+              <h1>
+                <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+              </h1>
+              <h2>{post.frontmatter.date}</h2>
+              <p>{post.excerpt}</p>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          excerpt(pruneLength: 250)
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+          }
+        }
+      }
+    }
+  }
+`;
